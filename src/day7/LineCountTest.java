a409b5f5b0e4139f04b156021546d5a35e84b895
laikrish <laikrish@IN-MUM-LDVM040.corp.capgemini.com>
2018-09-01 08:37:22 -0400

    .

---
+++ src/day7/LineCountTest.java
@@ -0,0 +1,24 @@
+package day7;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+import com.day7.LineCount;
+
+class LineCountTest {
+
+	@Test
+	void testLineCount() throws IOException {
+		
+		
+				
+	
+		
+
+		assertEquals(19,LineCount.countLines("C:\\day7\\MyCalculator.java"));
+}
+}
